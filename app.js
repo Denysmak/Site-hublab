22d6c6544848bbd17dad3137bbe5098fcc04ac09
Denysmak <denysmakene13@gmail.com>
2023-01-10 19:49:27 -0300

    adiciona home parte 1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let navLinkBotoesDois = document.querySelector('.nav-links-botoes-2');
 let navLista = document.querySelector('.nav-lista');
 
-
+//variavel que diz se a navlinkbotoes2 está aberta
 let listaAberta = false;
 
 
-
+//função que abre ou fecha a navlinkbotoes2
 navLista.addEventListener('click', () => {
     if(listaAberta == false){
         navLinkBotoesDois.style.display = 'flex';
@@ -18,8 +18,6 @@ navLista.addEventListener('click', () => {
 }
     else if(listaAberta == true){
         navLinkBotoesDois.style.display = 'none';
-        // navLinkBotoesDois.style.flexDirection = 'column';
-        // navLinkBotoesDois.style.fontSize = '5px';
         listaAberta = false; 
     }
 })
